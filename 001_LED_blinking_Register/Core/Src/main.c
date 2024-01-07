7c8bf0a3fcf56f9d5a50ca1baac62a6453fda880
omrvrl <109516713+omrvrl@users.noreply.github.com>
2024-01-08 01:00:46 +0300

    Update main.c

--- 001_LED_blinking_Register/Core/Src/main.c
+++ 001_LED_blinking_Register/Core/Src/main.c
@@ -121,13 +121,6 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-		RCC->APB2ENR |= (1<<2);
-
-
-		//HAL_Delay(1000);
-		//GPIOC->BSRR &= ~(1<<8);
-		//HAL_Delay(1000);
-
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
